ba3cfda1bd5b1591b276203955274de33ee3a91f
Lazzadi <lazarec.adrian@gmail.com>
2022-08-23 21:38:11 +0300

    app synchronizes folders with no subfolders

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import pathlib
+import shutil
 
 def timeModified(path):
 
@@ -18,10 +19,41 @@ def scanFolder(folder):
     content = {}
     for path, _, fileList in os.walk(folder):
         for fileName in fileList:
-            print(path + fileName)
             time = str(timeModified(path))
             content[fileName] = time
     return content
+
+# #Function copies changed files from Source to Replica
+# Three situations: New files, deleting files and changing files
+def synchronise(folderSource, folderReplica):
+    contentSource = scanFolder(folderSource)
+    contentReplica = scanFolder(folderReplica)
+    for fileNameSource, dateModifiedSource in contentSource.items():
+        found = False
+        for fileNameReplica, dateModifiedReplica in contentReplica.items():
+            if(fileNameSource == fileNameReplica):
+                found = True
+                if(dateModifiedSource != dateModifiedReplica):
+                    # print (folderSource + '/' + fileNameSource, folderReplica)
+                    shutil.copy2(folderSource + '/' + fileNameSource, folderReplica)
+        if found == False:
+            pass
+            shutil.copy2(folderSource + '/' + fileNameSource, folderReplica)
+
+
+
+        
+
+###
+#Scan Source
+#Scan Replica
+#Take one file in source and check if it can be found in replica
+    #if it can be found in replica check if it has the same timestamp. 
+    #if it has the same timestamp do nothing
+    #if it has a different timestamp find file in replica and delete it then add file in source to replica
+    #if it can't be found in Replica add file from source to replica
+    #to check for deleted items we will look in replica and compare to Source. If we have a file in Replica that is not in source, we remove that file from replica
+###                 
 
 # #Function writes changes in log folder
 # def logFolder(folder):
@@ -31,7 +63,5 @@ def scanFolder(folder):
 #     log.write('Hello World')
 #     log.close()
 
-# #Function copies changed files from Source to Replica
-# def synchronise(folderS, folderR):
-#     pass
+
 
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
-from functions import scanFolder, timeModified
+from functions import scanFolder, synchronise, timeModified
 import datetime
 import pathlib
+import shutil
 
 # print(timeModified('/home/l/Documents/repos/folder-sync/folder-sync/Source/log.txt'))
 
 
 
-print(scanFolder('/home/l/Documents'))
+# print(scanFolder('/home/l/Documents'))
 # print(timeModified('/home/l/Documents/repos/folder-sync/folder-sync/Source/log.txt'))
+
+synchronise('/home/l/Documents/repos/folder-sync/folder-sync/Source', '/home/l/Documents/repos/folder-sync/folder-sync/Replica')
+
+# print(scanFolder('/home/l/Documents/repos/folder-sync/folder-sync/Source'))
